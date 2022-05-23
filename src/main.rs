09b183a9174607df81a3fbf02e02910561d93226
Anna Melnikov <anna@caravelapp.com>
2022-05-23 13:42:34 -0500

    feat: copy the passphrase to clipboard
    Copy the generated passphrase to clipboard.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use password_generator;
+use cli_clipboard::{ClipboardContext, ClipboardProvider};
+use std::process;
 
 fn main() {
     let passphrase = password_generator::generate_passphrase();
-    println!("{}", passphrase);
+    println!("Your passphrase is: {}", passphrase);
+
+    let mut ctx = ClipboardContext::new().unwrap();
+    ctx.set_contents(passphrase).unwrap();
+    println!("Copied to clipboard! ✅");
+    process::exit(0);
 }
